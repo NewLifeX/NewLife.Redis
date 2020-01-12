0c205a154bd2da0a446029967b585b39e5bdb381
大石头 <nnhy@vip.qq.com>
2020-01-12 09:13:40 +0800

    增加 Queue/Stack 单元测试

---
+++ XUnitTest/QueueTests.cs
@@ -0,0 +1,63 @@
+﻿using NewLife.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class QueueTests
+    {
+        private FullRedis _redis;
+
+        public QueueTests()
+        {
+            _redis = new FullRedis("127.0.0.1:6379", null, 2);
+        }
+
+        [Fact]
+        public void Queue_Normal()
+        {
+            var key = "qkey";
+
+            // 删除已有
+            _redis.Remove(key);
+            var q = _redis.GetQueue<String>(key);
+            _redis.SetExpire(key, TimeSpan.FromMinutes(60));
+
+            var queue = q as RedisQueue<String>;
+            Assert.NotNull(queue);
+
+            // 取出个数
+            var count = queue.Count;
+            Assert.True(queue.IsEmpty);
+            Assert.Equal(0, count);
+
+            // 添加
+            var vs = new[] { "1234", "abcd", "新生命团队", "ABEF" };
+            q.Add(vs);
+
+            // 对比个数
+            var count2 = queue.Count;
+            Assert.False(queue.IsEmpty);
+            Assert.Equal(count + vs.Length, count2);
+
+            // 取出来
+            var vs2 = q.Take(2).ToArray();
+            Assert.Equal(2, vs2.Length);
+            Assert.Equal(vs[0], vs2[0]);
+            Assert.Equal(vs[1], vs2[1]);
+
+            var vs3 = q.Take(2).ToArray();
+            Assert.Equal(2, vs3.Length);
+            Assert.Equal(vs[2], vs3[0]);
+            Assert.Equal(vs[3], vs3[1]);
+
+            // 对比个数
+            var count3 = queue.Count;
+            Assert.True(queue.IsEmpty);
+            Assert.Equal(count, count3);
+        }
+    }
+}
---
+++ XUnitTest/StackTests.cs
@@ -0,0 +1,63 @@
+﻿using NewLife.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class StackTests
+    {
+        private FullRedis _redis;
+
+        public StackTests()
+        {
+            _redis = new FullRedis("127.0.0.1:6379", null, 2);
+        }
+
+        [Fact]
+        public void Stack_Normal()
+        {
+            var key = "skey";
+
+            // 删除已有
+            _redis.Remove(key);
+            var s = _redis.GetStack<String>(key);
+            _redis.SetExpire(key, TimeSpan.FromMinutes(60));
+
+            var stack = s as RedisStack<String>;
+            Assert.NotNull(stack);
+
+            // 取出个数
+            var count = stack.Count;
+            Assert.True(stack.IsEmpty);
+            Assert.Equal(0, count);
+
+            // 添加
+            var vs = new[] { "1234", "abcd", "新生命团队", "ABEF" };
+            s.Add(vs);
+
+            // 对比个数
+            var count2 = stack.Count;
+            Assert.False(stack.IsEmpty);
+            Assert.Equal(count + vs.Length, count2);
+
+            // 取出来
+            var vs2 = s.Take(2).ToArray();
+            Assert.Equal(2, vs2.Length);
+            Assert.Equal(vs[3], vs2[0]);
+            Assert.Equal(vs[2], vs2[1]);
+
+            var vs3 = s.Take(2).ToArray();
+            Assert.Equal(2, vs3.Length);
+            Assert.Equal(vs[1], vs3[0]);
+            Assert.Equal(vs[0], vs3[1]);
+
+            // 对比个数
+            var count3 = stack.Count;
+            Assert.True(stack.IsEmpty);
+            Assert.Equal(count, count3);
+        }
+    }
+}
