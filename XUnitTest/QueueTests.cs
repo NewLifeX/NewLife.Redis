42a15c7aca61ebc159e823f7474c787b66acf133
大石头 <nnhy@vip.qq.com>
2020-02-09 15:42:28 +0800

    多个测试函数可能并行执行，把key分开来

--- XUnitTest/QueueTests.cs
+++ XUnitTest/QueueTests.cs
@@ -19,32 +19,31 @@ namespace XUnitTest
         [Fact]
         public void Queue_Normal()
         {
-            var key = "qkey";
+            var key = "qkey_normal";
 
             // 删除已有
             _redis.Remove(key);
             var q = _redis.GetQueue<String>(key);
             _redis.SetExpire(key, TimeSpan.FromMinutes(60));
 
-            var queue = q as RedisQueue<String>;
-            Assert.NotNull(queue);
+            Assert.NotNull(q as RedisQueue<String>);
 
             // 取出个数
-            var count = queue.Count;
-            Assert.True(queue.IsEmpty);
+            var count = q.Count;
+            Assert.True(q.IsEmpty);
             Assert.Equal(0, count);
 
             // 添加
             var vs = new[] { "1234", "abcd", "新生命团队", "ABEF" };
-            queue.Add(vs);
+            q.Add(vs);
 
             // 对比个数
-            var count2 = queue.Count;
-            Assert.False(queue.IsEmpty);
+            var count2 = q.Count;
+            Assert.False(q.IsEmpty);
             Assert.Equal(count + vs.Length, count2);
 
             // 取出来
-            var vs2 = queue.Take(2).ToArray();
+            var vs2 = q.Take(2).ToArray();
             Assert.Equal(2, vs2.Length);
             Assert.Equal("1234", vs2[0]);
             Assert.Equal("abcd", vs2[1]);
@@ -59,15 +58,15 @@ namespace XUnitTest
             Assert.Null(vs3[4]);
 
             // 对比个数
-            var count3 = queue.Count;
-            Assert.True(queue.IsEmpty);
+            var count3 = q.Count;
+            Assert.True(q.IsEmpty);
             Assert.Equal(count, count3);
         }
 
         [Fact]
         public void Queue_Strict()
         {
-            var key = "qkey";
+            var key = "qkey_strict";
 
             // 删除已有
             _redis.Remove(key);
@@ -79,19 +78,19 @@ namespace XUnitTest
 
             _redis.Remove(queue.AckKey);
             queue.Strict = true;
-            Assert.Equal("qkey_ack", queue.AckKey);
+            Assert.Equal(key + "_ack", queue.AckKey);
 
             // 取出个数
-            var count = queue.Count;
-            Assert.True(queue.IsEmpty);
+            var count = q.Count;
+            Assert.True(q.IsEmpty);
             Assert.Equal(0, count);
 
             // 添加
             var vs = new[] { "1234", "abcd", "新生命团队", "ABEF" };
-            queue.Add(vs);
+            q.Add(vs);
 
             // 取出来
-            var vs2 = queue.Take(3).ToArray();
+            var vs2 = q.Take(3).ToArray();
             Assert.Equal(3, vs2.Length);
             Assert.Equal("1234", vs2[0]);
             Assert.Equal("abcd", vs2[1]);
@@ -111,6 +110,57 @@ namespace XUnitTest
             Assert.Equal("新生命团队", vs3[0]);
             Assert.Null(vs3[1]);
             Assert.Null(vs3[2]);
+        }
+
+        [Fact]
+        public void Queue_NotEnough()
+        {
+            var key = "qkey_not_enough";
+
+            // 删除已有
+            _redis.Remove(key);
+            var q = _redis.GetQueue<String>(key);
+            _redis.SetExpire(key, TimeSpan.FromMinutes(60));
+
+            var queue = q as RedisQueue<String>;
+            Assert.NotNull(queue);
+
+            // 取出个数
+            var count = q.Count;
+            Assert.True(q.IsEmpty);
+            Assert.Equal(0, count);
+
+            // 添加
+            var vs = new[] { "1234", "abcd" };
+            q.Add(vs);
+
+            // 取出来
+            var vs2 = q.Take(3).ToArray();
+            Assert.Equal(3, vs2.Length);
+            Assert.Equal("1234", vs2[0]);
+            Assert.Equal("abcd", vs2[1]);
+            Assert.Null(vs2[2]);
+
+            // 再取，这个时候已经没有元素
+            var vs4 = q.Take(3).ToArray();
+            Assert.Equal(3, vs4.Length);
+            Assert.Null(vs4[0]);
+            Assert.Null(vs4[1]);
+            Assert.Null(vs4[2]);
+
+            // 管道批量获取
+            var vs3 = q.Take(5).ToArray();
+            Assert.Equal(5, vs3.Length);
+            Assert.Null(vs3[0]);
+            Assert.Null(vs3[1]);
+            Assert.Null(vs3[2]);
+            Assert.Null(vs3[3]);
+            Assert.Null(vs3[4]);
+
+            // 对比个数
+            var count3 = q.Count;
+            Assert.True(q.IsEmpty);
+            Assert.Equal(count, count3);
         }
     }
 }
