b98ee9edd6214f48841e9a44435eb76e85f78fea
大石头 <nnhy@vip.qq.com>
2020-07-21 08:42:47 +0800

    Redis延迟队列，key放在zset，消息体放在kv

---
+++ NewLife.Redis/RedisDelayQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLife.Caching
+{
+    /// <summary>Redis延迟队列，key放在zset，消息体放在kv</summary>
+    public class RedisDelayQueue
+    {
+    }
+}
