b6876a7dc8ac6145908986cbbcddf8a80fb650c1
大石头 <nnhy@vip.qq.com>
2020-09-17 23:17:00 +0800

    封装Geo实现

---
+++ NewLife.Redis/Models/GeoInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NewLife.Caching.Models
+{
+    /// <summary>地理坐标</summary>
+    public class GeoInfo
+    {
+        #region 属性
+        /// <summary>名称</summary>
+        public String Name { get; set; }
+
+        /// <summary>经度</summary>
+        public Double Longitude { get; set; }
+
+        /// <summary>纬度</summary>
+        public Double Latitude { get; set; }
+
+        /// <summary>距离</summary>
+        public Double Distance { get; set; }
+        #endregion
+    }
+}
---
+++ NewLife.Redis/RedisGeo.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using NewLife.Caching.Models;
+using NewLife.Data;
+
+namespace NewLife.Caching
+{
+    /// <summary>地理信息数据</summary>
+    /// <remarks>
+    /// 将指定的地理空间位置（纬度、经度、名称）添加到指定的key中。
+    /// 这些数据将会存储到sorted set这样的目的是为了方便使用GEORADIUS或者GEORADIUSBYMEMBER命令对数据进行半径查询等操作。
+    /// 该命令以采用标准格式的参数x,y,所以经度必须在纬度之前。这些坐标的限制是可以被编入索引的，区域面积可以很接近极点但是不能索引。
+    /// </remarks>
+    public class RedisGeo : RedisBase
+    {
+        #region 实例化
+        /// <summary>实例化地理信息</summary>
+        /// <param name="redis"></param>
+        /// <param name="key"></param>
+        public RedisGeo(Redis redis, String key) : base(redis, key) { }
+        #endregion
+
+        /// <summary>添加</summary>
+        /// <param name="name"></param>
+        /// <param name="longitude"></param>
+        /// <param name="latitude"></param>
+        /// <returns></returns>
+        public Int32 Add(String name, Double longitude, Double latitude) => Execute(rc => rc.Execute<Int32>("GEOADD", Key, longitude, latitude, name), true);
+
+        /// <summary>添加</summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public Int32 Add(params GeoInfo[] items)
+        {
+            var args = new List<Object> { Key };
+            foreach (var item in items)
+            {
+                args.Add(item.Longitude);
+                args.Add(item.Latitude);
+                args.Add(item.Name);
+            }
+            return Execute(rc => rc.Execute<Int32>("GEOADD", args.ToArray()), true);
+        }
+
+        /// <summary>两点距离</summary>
+        /// <param name="from">开始点</param>
+        /// <param name="to">结束点</param>
+        /// <param name="unit">单位。m/km/mi/ft</param>
+        /// <returns></returns>
+        public Double GetDistance(String from, String to, String unit = null)
+        {
+            return unit.IsNullOrEmpty() ?
+                Execute(rc => rc.Execute<Double>("GEODIST", Key, from, to), false) :
+                Execute(rc => rc.Execute<Double>("GEODIST", Key, from, to, unit), false);
+        }
+
+        /// <summary>获取一批点的坐标</summary>
+        /// <param name="members"></param>
+        /// <returns></returns>
+        public GeoInfo[] GetPosition(params String[] members)
+        {
+            var args = new List<Object> { Key };
+            foreach (var item in members)
+            {
+                args.Add(item);
+            }
+
+            var rs = Execute(rc => rc.Execute<Object[]>("GEOPOS", args.ToArray()), false);
+            if (rs == null || rs.Length == 0) return null;
+
+            var list = new List<GeoInfo>();
+            for (var i = 0; i < rs.Length; i++)
+            {
+                var inf = new GeoInfo { Name = members[i] };
+                if (rs[i] is Object[] vs)
+                {
+                    inf.Longitude = (vs[0] as Packet).ToStr().ToDouble();
+                    inf.Latitude = (vs[1] as Packet).ToStr().ToDouble();
+                }
+
+                list.Add(inf);
+            }
+
+            return list.ToArray();
+        }
+
+        /// <summary>以给定的经纬度为中心， 返回键包含的位置元素当中， 与中心的距离不超过给定最大距离的所有位置元素</summary>
+        /// <param name="longitude"></param>
+        /// <param name="latitude"></param>
+        /// <param name="radius"></param>
+        /// <param name="unit"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public GeoInfo[] GetRadius(Double longitude, Double latitude, Double radius, String unit = null, Int32 count = 0)
+        {
+            var rs = count > 0 ?
+                Execute(rc => rc.Execute<Object[]>("GEORADIUS", Key, longitude, latitude, radius, unit, "WITHDIST", "WITHCOORD", "COUNT", count), false) :
+                Execute(rc => rc.Execute<Object[]>("GEORADIUS", Key, longitude, latitude, radius, unit, "WITHDIST", "WITHCOORD"), false);
+            if (rs == null || rs.Length == 0) return null;
+
+            var list = new List<GeoInfo>();
+            for (var i = 0; i < rs.Length; i++)
+            {
+                var inf = new GeoInfo();
+                if (rs[i] is Object[] vs)
+                {
+                    inf.Name = (vs[0] as Packet).ToStr();
+                    inf.Distance = (vs[1] as Packet).ToStr().ToDouble();
+                    if (vs[2] is Object[] vs2)
+                    {
+                        inf.Longitude = (vs2[0] as Packet).ToStr().ToDouble();
+                        inf.Latitude = (vs2[1] as Packet).ToStr().ToDouble();
+                    }
+                }
+
+                list.Add(inf);
+            }
+
+            return list.ToArray();
+        }
+
+        /// <summary>以给定的点位为中心， 返回键包含的位置元素当中， 与中心的距离不超过给定最大距离的所有位置元素</summary>
+        /// <param name="member"></param>
+        /// <param name="radius"></param>
+        /// <param name="unit"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public GeoInfo[] GetRadius(string member, Double radius, String unit = null, Int32 count = 0)
+        {
+            var rs = count > 0 ?
+                Execute(rc => rc.Execute<Object[]>("GEORADIUSBYMEMBER", Key, member, radius, unit, "WITHDIST", "WITHCOORD", "COUNT", count), false) :
+                Execute(rc => rc.Execute<Object[]>("GEORADIUSBYMEMBER", Key, member, radius, unit, "WITHDIST", "WITHCOORD"), false);
+            if (rs == null || rs.Length == 0) return null;
+
+            var list = new List<GeoInfo>();
+            for (var i = 0; i < rs.Length; i++)
+            {
+                var inf = new GeoInfo();
+                if (rs[i] is Object[] vs)
+                {
+                    inf.Name = (vs[0] as Packet).ToStr();
+                    inf.Distance = (vs[1] as Packet).ToStr().ToDouble();
+                    if (vs[2] is Object[] vs2)
+                    {
+                        inf.Longitude = (vs2[0] as Packet).ToStr().ToDouble();
+                        inf.Latitude = (vs2[1] as Packet).ToStr().ToDouble();
+                    }
+                }
+
+                list.Add(inf);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
