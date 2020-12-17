766e24d6c672a947e2c3ce4472cc8480a97e5084
大石头 <nnhy@vip.qq.com>
2020-12-18 00:03:49 +0800

    pubsub代码整理

--- NewLife.Redis/PubSub.cs
+++ NewLife.Redis/PubSub.cs
@@ -19,54 +19,55 @@ namespace NewLife.Caching
         public PubSub(Redis redis, String key) : base(redis, key) { }
         #endregion
 
-        /// <summary>订阅给定的模式</summary>
-        /// <remarks>
-        /// 支持的模式(patterns)有:
-        /// h? llo subscribes to hello, hallo and hxllo
-        /// h* llo subscribes to hllo and heeeello
-        /// h[ae]llo subscribes to hello and hallo, but not hillo
-        /// 如果想输入普通的字符，可以在前面添加\
-        /// </remarks>
-        /// <param name="patterns"></param>
-        /// <returns></returns>
-        public Int32 PSubscribe(params String[] patterns) =>
-            //var args = new List<Object>
-            //{
-            //    Key
-            //};
-            //foreach (var item in patterns)
-            //{
-            //    args.Add(item);
-            //}
-            //return Execute(rc => rc.Execute<Int32>("PSUBSCRIBE", args.ToArray()), true);
-            Execute(rc => rc.Execute<Int32>("PSUBSCRIBE", patterns), true);
+        ///// <summary>订阅给定的模式</summary>
+        ///// <remarks>
+        ///// 支持的模式(patterns)有:
+        ///// h? llo subscribes to hello, hallo and hxllo
+        ///// h* llo subscribes to hllo and heeeello
+        ///// h[ae]llo subscribes to hello and hallo, but not hillo
+        ///// 如果想输入普通的字符，可以在前面添加\
+        ///// </remarks>
+        ///// <param name="patterns"></param>
+        ///// <returns></returns>
+        //public Int32 PSubscribe(params String[] patterns) =>
+        //    //var args = new List<Object>
+        //    //{
+        //    //    Key
+        //    //};
+        //    //foreach (var item in patterns)
+        //    //{
+        //    //    args.Add(item);
+        //    //}
+        //    //return Execute(rc => rc.Execute<Int32>("PSUBSCRIBE", args.ToArray()), true);
+        //    Execute(rc => rc.Execute<Int32>("PSUBSCRIBE", patterns), true);
 
-        /// <summary>指示客户端退订指定模式，若果没有提供模式则退出所有模式</summary>
-        /// <returns></returns>
-        public Int32 PUnSubscribe(params String[] patterns)
-        {
-            if (patterns != null && patterns.Length > 0)
-                return Execute(rc => rc.Execute<Int32>("PUNSUBSCRIBE", patterns), true);
-            else
-                return Execute(rc => rc.Execute<Int32>("PUNSUBSCRIBE"), true);
-        }
+        ///// <summary>指示客户端退订指定模式，若果没有提供模式则退出所有模式</summary>
+        ///// <returns></returns>
+        //public Int32 PUnSubscribe(params String[] patterns)
+        //{
+        //    if (patterns != null && patterns.Length > 0)
+        //        return Execute(rc => rc.Execute<Int32>("PUNSUBSCRIBE", patterns), true);
+        //    else
+        //        return Execute(rc => rc.Execute<Int32>("PUNSUBSCRIBE"), true);
+        //}
 
-        /// <summary>订阅给指定频道的信息</summary>
-        /// <param name="channels"></param>
-        /// <returns></returns>
-        public Int32 Subscribe(params String[] channels) => Execute(rc => rc.Execute<Int32>("SUBSCRIBE", channels), true);
+        ///// <summary>订阅给指定频道的信息</summary>
+        ///// <param name="channels"></param>
+        ///// <returns></returns>
+        //public Int32 Subscribe(params String[] channels) => Execute(rc => rc.Execute<Int32>("SUBSCRIBE", channels), true);
 
 #if !NET40
         /// <summary>订阅大循环</summary>
         /// <param name="onMessage"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public async Task SubscribeAsync(Action<String, String, String> onMessage, CancellationToken cancellationToken)
+        public async Task SubscribeAsync(Action<String, String> onMessage, CancellationToken cancellationToken)
         {
             var client = Redis.Pool.Get();
             client.Reset();
 
-            await client.ExecuteAsync<String[]>("SUBSCRIBE", new Object[] { Key });
+            var channels = Key.Split(",", ";").Cast<Object>().ToArray();
+            await client.ExecuteAsync<String[]>("SUBSCRIBE", channels);
 
             while (!cancellationToken.IsCancellationRequested)
             {
@@ -75,30 +76,32 @@ namespace NewLife.Caching
 
                 //var rs = await client.ExecuteAsync<String[]>(null, new Object[] { new Object() }, source2.Token);
                 var rs = await client.ReadMoreAsync<String[]>(source2.Token);
-                if (rs.Length == 3) onMessage(rs[0], rs[1], rs[2]);
+                if (rs.Length == 3 && rs[0] == "message") onMessage(rs[1], rs[2]);
             }
+           
+            await client.ExecuteAsync<String[]>("SUBSCRIBE", channels);
 
             Redis.Pool.Put(client);
         }
 #endif
 
-        /// <summary>退订给定的频道</summary>
-        /// <returns></returns>
-        public Int32 UnSubscribe(params String[] channels)
-        {
-            if (channels != null && channels.Length > 0)
-                return Execute(rc => rc.Execute<Int32>("UNSUBSCRIBE", channels), true);
-            else
-                return Execute(rc => rc.Execute<Int32>("UNSUBSCRIBE"), true);
-        }
+        ///// <summary>退订给定的频道</summary>
+        ///// <returns></returns>
+        //public Int32 UnSubscribe(params String[] channels)
+        //{
+        //    if (channels != null && channels.Length > 0)
+        //        return Execute(rc => rc.Execute<Int32>("UNSUBSCRIBE", channels), true);
+        //    else
+        //        return Execute(rc => rc.Execute<Int32>("UNSUBSCRIBE"), true);
+        //}
 
-        /// <summary>发布</summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <summary>发布消息</summary>
+        /// <param name="message">消息内容</param>
+        /// <returns>返回接收到消息的客户端个数</returns>
         public Int32 Publish(String message) => Execute(rc => rc.Execute<Int32>("PUBLISH", Key, message), true);
 
-        /// <summary>自省</summary>
-        /// <returns></returns>
-        public Int32 Pubsub() => 0;
+        ///// <summary>自省</summary>
+        ///// <returns></returns>
+        //public Int32 Pubsub() => 0;
     }
 }
--- XUnitTest/PubSubTests.cs
+++ XUnitTest/PubSubTests.cs
@@ -22,16 +22,18 @@ namespace XUnitTest
             _redis = rds;
         }
 
-        [Fact]
+        [Fact(DisplayName = "单通道定义")]
         public void Subscribe()
         {
             var pb = new PubSub(_redis, "pb_test");
 
             var source = new CancellationTokenSource(2_000);
 
-            Task.Run(() => pb.SubscribeAsync((m, t, s) =>
+            var count = 0;
+            Task.Run(() => pb.SubscribeAsync((t, s) =>
             {
-                XTrace.WriteLine("Consume: {0}[{1}] {2}", m, t, s);
+                count++;
+                XTrace.WriteLine("Consume: [{0}] {1}", t, s);
             }, source.Token));
 
             Thread.Sleep(100);
@@ -46,6 +48,40 @@ namespace XUnitTest
             pb.Publish("test3");
 
             Thread.Sleep(100);
+
+            Assert.Equal(3, count);
+        }
+
+        [Fact(DisplayName = "多通道定义")]
+        public void Subscribes()
+        {
+            var pb = new PubSub(_redis, "pb_t1,pb_t2");
+
+            var source = new CancellationTokenSource(2_000);
+
+            var count = 0;
+            Task.Run(() => pb.SubscribeAsync((t, s) =>
+            {
+                count++;
+                XTrace.WriteLine("Consume: [{0}] {1}", t, s);
+            }, source.Token));
+
+            Thread.Sleep(100);
+
+            var pb1 = new PubSub(_redis, "pb_t1");
+            var rs = pb1.Publish("test");
+            Assert.Equal(1, rs);
+
+            Thread.Sleep(100);
+            var pb2 = new PubSub(_redis, "pb_t2");
+            pb2.Publish("test2");
+
+            Thread.Sleep(100);
+            pb2.Publish("test3");
+
+            Thread.Sleep(100);
+
+            Assert.Equal(3, count);
         }
 
         [Fact]
