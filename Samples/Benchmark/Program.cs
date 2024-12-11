cc52acf2f6d79b35407d76bcc541cf144d51720c
大石头 <nnhy@vip.qq.com>
2024-12-11 10:21:42 +0800

    引入 BenchmarkDotNet

--- Samples/Benchmark/Program.cs
+++ Samples/Benchmark/Program.cs
@@ -1,4 +1,6 @@
-﻿using NewLife.Caching;
+﻿using Benchmark;
+using BenchmarkDotNet.Running;
+using NewLife.Caching;
 using Stardust;
 
 //!!! 标准后台服务项目模板，新生命团队强烈推荐
@@ -13,14 +15,6 @@ services.AddSingleton(XTrace.Log);
 // 配置星尘。自动读取配置文件 config/star.config 中的服务器地址
 var star = services.AddStardust();
 
-var rds = new FullRedis
-{
-    Tracer = star.Tracer,
-    Log = XTrace.Log,
-};
-rds.Init("server=127.0.0.1:6379;password=;db=3;timeout=5000");
+var summary = BenchmarkRunner.Run<BasicBenchmark>();
 
-var host = services.BuildHost();
-
-// 异步阻塞，友好退出
-await host.RunAsync();
+Console.ReadLine();
