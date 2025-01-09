2187f0153d6c9a05e9293c79943620ee9e2e7950
智能大石头 <nnhy@vip.qq.com>
2025-01-09 23:18:46 +0800

    修改全局Utility扩展的命名空间为NewLife

--- XUnitTest/Queues/StreamTests.cs
+++ XUnitTest/Queues/StreamTests.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using NewLife;
 using NewLife.Caching;
 using NewLife.Caching.Queues;
 using NewLife.Log;
