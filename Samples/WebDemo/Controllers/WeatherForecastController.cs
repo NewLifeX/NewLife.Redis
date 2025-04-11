d533bc9bea95b955b6cd7b6b103a6b7068e21215
智能大石头 <nnhy@vip.qq.com>
2025-04-12 01:32:50 +0800

    新增WebDemo项目，演示如何在WebApi中使用Redis

---
+++ Samples/WebDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewLife;
+using NewLife.Caching;
+
+namespace WebDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    private readonly ICacheProvider _cacheProvider;
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ICacheProvider cacheProvider, ILogger<WeatherForecastController> logger)
+    {
+        _cacheProvider = cacheProvider;
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var cache = _cacheProvider.Cache;
+        var v = cache.Get<String>("test");
+        if (v.ToInt() <= 0) cache.Remove("test");
+        cache.SetExpire("test", TimeSpan.FromSeconds(60));
+
+        var v2 = _cacheProvider.Cache.Increment("test", 1);
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = (Int32)v2,
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ Samples/WebDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using NewLife.Log;
+
+XTrace.UseConsole();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRedisCacheProvider();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
